ed08f6033090bc1d8441e8024cfa278510c7d6ad
Omkar Pathak <omkarpathak27@gmail.com>
2019-04-08 15:30:17 +0530

    Added timeseries chart

--- poc/range_slider/views.py
+++ poc/range_slider/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import json
 from .models import TrendTable
 from django.contrib import messages
+import datetime
 
 # Create your views here.
 def homepage(request):
@@ -28,5 +29,38 @@ def set_trends(request):
     messages.success(request, 'Entry added successfully!')
     return render(request, 'set_trends.html')
 
+def get_datetime_object(date, format='%Y-%m-%d'):
+    object = datetime.datetime.strptime(date, format).date()
+    return object
+
 def trends(request):
-    return render(request, 'trends.html')
+    if request.method == 'POST':
+        to_date = request.POST.get('to_date')
+        from_date = request.POST.get('from_date')
+
+        # get all data
+        result = TrendTable.objects.filter(
+                    date_added__range=[
+                                        get_datetime_object(to_date), 
+                                        get_datetime_object(from_date) + datetime.timedelta(days=1)
+                        ]
+                    )
+        
+        # fetch only mean values from all entries
+        results = []
+        for res in result:
+            date = int(res.date_added.strftime("%H"))
+            results.append([date, res.slider_one])
+            results.append([date, res.slider_two])
+            results.append([date, res.slider_three])
+            results.append([date, res.slider_four])
+            results.append([date, res.slider_five])
+
+        context = {
+            'to_date': to_date,
+            'from_date': from_date,
+            'result': results
+        }
+        return render(request, 'trends.html', context=context)
+    else:
+        return render(request, 'trends.html')
