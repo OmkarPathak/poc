c59cd5d490c8625bed16db2cdc5391c1050175b2
Omkar Pathak <omkarpathak27@gmail.com>
2019-04-20 10:00:34 +0530

    Added MySQL compatibility

--- poc/poc/settings.py
+++ poc/poc/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'poc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
